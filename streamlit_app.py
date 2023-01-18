87205ba0414ec7f90d06b353383cf61c481c044e
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-18 12:31:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import necessary libraries
+import openai
 import streamlit as st
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
